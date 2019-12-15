ff85dd2cceb6b81162f0fb4e6c8c5396cf6d0f97
jack <690137092@qq.com>
2019-12-15 13:52:45 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Layout from '../layout'
-
+import CommonIndex from '@/layout/common'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,8 +9,26 @@ const routes = [
     path: '/',
     name: 'Layout',
     component: Layout
+  },
+  {
+    path: '/commonIndex',
+    name: 'CommonIndex',
+    component: CommonIndex,
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/Hr/dashboard/index'),
+        name: 'Dashboard',
+        meta: { title: '首页', icon: 'dashboard', affix: true }
+      }
+    ]
   }
 ]
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 const router = new VueRouter({
   mode: 'history',
